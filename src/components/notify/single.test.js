3f2b0e72fdf4a94228ec380200d9104b3ac8d670
Rodrigo Nehring <rodrigonehring@hotmail.com>
2016-10-04 17:52:12 -0300

    linter

--- src/components/notify/single.test.js
+++ src/components/notify/single.test.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import { shallow, mount, render } from 'enzyme';
+import { shallow } from 'enzyme';
 import { expect } from 'chai';
 
 import Single from './single';
 
-describe("single.js component", function() {
-
-  it("contains a msg passed by props", function() {
-    const single = shallow(<Single msg='umamensagem' />);
+describe('single.js component', () => {
+  it('contains a msg passed by props', () => {
+    const single = shallow(<Single msg="umamensagem" />);
     expect(single.html()).to.contain('umamensagem');
   });
 
   it('change css class when change props stateTransition - closing', () => {
-    const single = shallow(<Single transitionState='closing' />);
+    const single = shallow(<Single transitionState="closing" />);
     expect(single.html()).to.contain('closing');
   });
 
   it('change css class when change props stateTransition - opened', () => {
-    const single = shallow(<Single transitionState='opened' />);
+    const single = shallow(<Single transitionState="opened" />);
     expect(single.html()).to.contain('opened');
   });
-
 });
